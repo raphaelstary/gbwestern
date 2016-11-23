dd09925a5576baffc782d9f4d47464190d6fd4b2
raphaelstary <raphael.stary@gmail.com>
2016-11-23 10:11:36 +0100

    integrating new audio bootstrapper

--- www/js/mygame/core/MyGameResources.js
+++ www/js/mygame/core/MyGameResources.js
@@ -1,5 +1,5 @@
-G.MyGameResources = (function (AtlasResourceHelper, DeviceInfo, userAgent, createAtlasPaths, File, UI, Sound, Object,
-    AudioManager) {
+G.MyGameResources = (function (AtlasResourceHelper, DeviceInfo, userAgent, createAtlasPaths, File, UI, Sound,
+    SoundResources) {
     "use strict";
 
     // your files
@@ -12,14 +12,7 @@ G.MyGameResources = (function (AtlasResourceHelper, DeviceInfo, userAgent, creat
             createAtlasPaths().add(UI.HEIGHT).getResolver());
 
         scenes = resourceLoader.addJSON(File.SCENES);
-
-        var keys = Object.keys(Sound);
-        sounds = keys.map(function (soundKey) {
-            return resourceLoader.addAudio(File.SOUND_PATH + Sound[soundKey] + File.SOUND_FORMAT);
-        }).reduce(function (soundDict, sound, index) {
-            soundDict[Sound[keys[index]]] = sound;
-            return soundDict;
-        }, {});
+        sounds = new SoundResources(resourceLoader).createHtmlAudioSounds(Sound);
 
         return resourceLoader.getCount(); // number of registered files
     }
@@ -30,7 +23,7 @@ G.MyGameResources = (function (AtlasResourceHelper, DeviceInfo, userAgent, creat
             // services created with downloaded files
             gfxCache: AtlasResourceHelper.processLowRez(atlases, UI.WIDTH, UI.HEIGHT),
             scenes: scenes,
-            sounds: new AudioManager(sounds)
+            sounds: sounds
         };
     }
 
@@ -38,5 +31,5 @@ G.MyGameResources = (function (AtlasResourceHelper, DeviceInfo, userAgent, creat
         create: registerFiles,
         process: processFiles
     };
-})(H5.AtlasResourceHelper, H5.Device, window.navigator.userAgent, H5.createAtlasPaths, G.File, G.UI, G.Sound, Object,
-    H5.HtmlAudioManager);
+})(H5.AtlasResourceHelper, H5.Device, window.navigator.userAgent, H5.createAtlasPaths, G.File, G.UI, G.Sound,
+    H5.SoundResources);
